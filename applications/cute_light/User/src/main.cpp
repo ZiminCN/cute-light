1dbd612b8cfbc745c2cd5eb89b1c7e71c9a6ce87
Violet-Zimin <jianming.zeng@directdrivetech.com>
2025-11-18 18:51:09 +0800

    trival: use uf2 firmware make boards run!
    Signed-off-by: Violet-Zimin <jianming.zeng@directdrivetech.com>

--- applications/cute_light/User/src/main.cpp
+++ applications/cute_light/User/src/main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
+#include "zephyr/kernel.h"
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    printk("Cute Light Initialized!\n");
+
+    while(1)
+    {
+        k_sleep(K_MSEC(1000));
+        printk("Cute Light Running...\n");
+    }
+
     return 0;
 }
