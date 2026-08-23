365c5e1e70405843ad302372e32ba08640ba4476
zimin <18171190533@163.com>
2026-08-23 17:16:49 +0800

    trival: add usb cdc-acm and output is okay
    Signed-off-by: zimin <18171190533@163.com>

--- applications/cute_light/User/src/main.cpp
+++ applications/cute_light/User/src/main.cpp
@@ -1,12 +1,26 @@
 #include <iostream>
 #include "zephyr/kernel.h"
+#include "zephyr/usb/usbd.h"
+#include "zephyr/drivers/uart.h"
+
+#define CDC_ACM_DEVICE_NODE DT_CHOSEN(zephyr_console)
 
 int main()
 {
-    printk("Cute Light Initialized!\n");
+    const struct device *uart_dev = DEVICE_DT_GET(CDC_ACM_DEVICE_NODE);
+    
+    /* 等待设备就绪 */
+    while (!device_is_ready(uart_dev)) {
+        k_sleep(K_MSEC(100));
+    }
 
-    while(1)
-    {
+    /* 尝试输出 */
+    for (int i = 0; i < 10; i++) {
+        printk("Cute Light Initialized! %d\n", i);
+        k_sleep(K_MSEC(100));
+    }
+
+    while (1) {
         k_sleep(K_MSEC(1000));
         printk("Cute Light Running...\n");
     }
